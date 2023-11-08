5788a1fb6be018b9ae30b82991b53d6126b42c41
JiteraDeveloper <jitera-developer@jitera.com>
2023-11-08 16:39:13 +0000

    feat: Implement business logic 'Update user logged-in status'

--- repositories/UserRepository.js
+++ repositories/UserRepository.js
@@ -1,13 +1,23 @@
-const BaseRepository = require('./BaseRepository')
-
+const BaseRepository = require('./BaseRepository');
+const ApiError = require('../errors/ApiError');
 class UserRepository extends BaseRepository {
-  constructor () {
-    super('user')
-
-    this.DEFAULT_LIMIT = 20
-    this.DEFAULT_SORT = [['id', 'DESC']]
-    this.DEFAULT_PAGE = 0
+  constructor() {
+    super('user');
+    this.DEFAULT_LIMIT = 20;
+    this.DEFAULT_SORT = [['id', 'DESC']];
+    this.DEFAULT_PAGE = 0;
+  }
+  async updateLoggedInStatus(user_id, logged_in) {
+    try {
+      const user = await this.findById(user_id);
+      if (!user) {
+        throw new ApiError('User not found', 404);
+      }
+      await this.update(user_id, { logged_in });
+      return { message: 'Logged-in status updated successfully.' };
+    } catch (error) {
+      throw new ApiError(error.message, error.status || 500);
+    }
   }
 }
-
-module.exports = new UserRepository()
+module.exports = new UserRepository();
