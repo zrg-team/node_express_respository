9a29f29a9f5adc0f82e02dc4dc61f74729f08bae
JiteraDeveloper <jitera-developer@jitera.com>
2023-11-09 19:09:24 +0000

    feat[business-logic]: implement 'Go back to article list' ## Changes: + '/repositories/UserRepository.js': Explain: + Write a function named "findArticlesByU... + '/routes/user.js': Explain: + Add a new route for the "getArticles" f... + '/controllers/UserController.js': Explain: + Write a function named "getArticles" in...

--- repositories/UserRepository.js
+++ repositories/UserRepository.js
@@ -1,13 +1,46 @@
 const BaseRepository = require('./BaseRepository')
-
+const { Article } = require('../models')
+const { Op } = require('sequelize')
 class UserRepository extends BaseRepository {
   constructor () {
     super('user')
-
-    this.DEFAULT_LIMIT = 20
-    this.DEFAULT_SORT = [['id', 'DESC']]
+    this.DEFAULT_LIMIT = 10
+    this.DEFAULT_SORT = [['created_at', 'DESC']]
     this.DEFAULT_PAGE = 0
   }
+  async findArticlesByUserId(user_id, page = this.DEFAULT_PAGE) {
+    try {
+      const offset = page * this.DEFAULT_LIMIT
+      const articles = await Article.findAndCountAll({
+        where: { user_id },
+        order: this.DEFAULT_SORT,
+        limit: this.DEFAULT_LIMIT,
+        offset: offset
+      })
+      const totalPages = Math.ceil(articles.count / this.DEFAULT_LIMIT)
+      const result = {
+        articles: articles.rows.map(article => {
+          return {
+            ...article.dataValues,
+            title: this.trimText(article.dataValues.title, 2),
+            description: this.trimText(article.dataValues.description, 2)
+          }
+        }),
+        totalItems: articles.count,
+        totalPages: totalPages
+      }
+      return result
+    } catch (error) {
+      throw error
+    }
+  }
+  trimText(text, lineCount) {
+    const MAX_CHAR_PER_LINE = 50
+    const MAX_CHAR = lineCount * MAX_CHAR_PER_LINE
+    if (text.length > MAX_CHAR) {
+      return text.substring(0, MAX_CHAR) + '...'
+    }
+    return text
+  }
 }
-
 module.exports = new UserRepository()
