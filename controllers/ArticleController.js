4ec3f9484b728508734a114de4a6fb7b39d7d3ae
JiteraDeveloper <jitera-developer@jitera.com>
2023-11-09 08:46:35 +0000

    feat: Implement business logic 'Get article detail'

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -1,5 +1,6 @@
-const ArticleRepository = require('../repositories/ArticleRepository');
-const response = require('../utils/response');
+const UserRepository = require('../repositories/UserRepository')
+const ArticleRepository = require('../repositories/ArticleRepository')
+const response = require('../utils/response')
 class ArticleController {
   async getArticles(req, res, next) {
     try {
@@ -14,5 +15,28 @@ class ArticleController {
       return next(err);
     }
   }
+  async getArticleDetail(req, res, next) {
+    try {
+      const { user_id, article_id } = req.body
+      // Verify user
+      const user = await UserRepository.findById(user_id)
+      if (!user) {
+        return response(res).failure('User not found', 404)
+      }
+      // Fetch article detail
+      const article = await ArticleRepository.findById(article_id)
+      if (!article) {
+        return response(res).failure('Article not found', 404)
+      }
+      // Return article detail
+      return response(res).success({
+        title: article.title,
+        description: article.description,
+        created_at: article.created_at
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
 }
-module.exports = ArticleController;
+module.exports = ArticleController
