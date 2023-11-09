01f824ec0a4de16b09fdac37877afeb4bc43cb54
JiteraDeveloper <jitera-developer@jitera.com>
2023-11-09 04:58:06 +0000

    feat: Implement business logic 'Go back to article list'

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,13 +1,10 @@
 const validateArticleListParams = (req, res, next) => {
   const { page, limit } = req.query;
-  if (!Number.isInteger(Number(page))) {
-    return res.status(422).json({ message: 'Wrong format.' });
+  if (!Number.isInteger(Number(page)) || Number(page) <= 0) {
+    return res.status(422).json({ message: 'Page must be an integer and greater than 0.' });
   }
   if (!Number.isInteger(Number(limit))) {
-    return res.status(422).json({ message: 'Wrong format.' });
-  }
-  if (Number(page) <= 0) {
-    return res.status(422).json({ message: 'Page must be greater than 0.' });
+    return res.status(422).json({ message: 'Limit must be an integer.' });
   }
   next();
 };
--- repositories/ArticleRepository.js
+++ repositories/ArticleRepository.js
@@ -7,5 +7,28 @@ class ArticleRepository extends BaseRepository {
   async get(articleId) {
     return this.findById(articleId);
   }
+  async getByUserId(user_id, page = 1, limit = 10) {
+    const offset = (page - 1) * limit;
+    const query = { where: { user_id }, order: { created_at: 'DESC' }, skip: offset, take: limit };
+    const [items, total] = await this.find(query);
+    const totalPages = Math.ceil(total / limit);
+    return { items: items.map(item => ({...item, title: this.trimText(item.title, 2), description: this.trimText(item.description, 2)})), total, totalPages };
+  }
+  trimText(text, lineCount) {
+    const words = text.split(' ');
+    let trimmedText = '';
+    let line = 1;
+    for (let i = 0; i < words.length; i++) {
+      if ((trimmedText + words[i]).length > lineCount * 50) {
+        if (line === lineCount) {
+          return trimmedText + '...';
+        }
+        line++;
+        trimmedText += '\n';
+      }
+      trimmedText += ' ' + words[i];
+    }
+    return trimmedText;
+  }
 }
 module.exports = new ArticleRepository();
