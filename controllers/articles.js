0ff2bf41c81bdea07a019fa13e8507baa03c9506
JiteraDeveloper <jitera-developer@jitera.com>
2023-11-09 05:20:39 +0000

    feat: Implement business logic 'Get article list'

--- controllers/articles.js
+++ controllers/articles.js
@@ -4,7 +4,7 @@ const response = require('../utils/response');
 const ApiError = require('../utils/ApiError');
 const auth = require('../libs/auth');
 const ArticleRepository = require('../repositories/ArticleRepository');
-async function getArticleDetail(req, res) {
+async function getArticleListByUser(req, res) {
   try {
     // Verify if the user is logged in
     const token = req.headers.authorization.split(' ')[1];
@@ -13,19 +13,28 @@ async function getArticleDetail(req, res) {
         throw new ApiError('User is not authenticated', 401);
       }
     });
-    const articleId = req.params.article_id;
-    if (isNaN(articleId)) {
-      throw new ApiError('Wrong format.', 422);
+    const userId = req.params.user_id;
+    const page = req.query.page || 1;
+    const limit = req.query.limit || 10;
+    const offset = (page - 1) * limit;
+    // Get articles by user id
+    const articles = await ArticleRepository.getByUserId(userId, limit, offset);
+    if (!articles) {
+      throw new ApiError('No articles found for this user', 404);
     }
-    const article = await ArticleRepository.get(articleId);
-    if (!article) {
-      throw new ApiError('This article is not found', 404);
-    }
-    // Return additional article details
+    // Trim title and description
+    articles.forEach(article => {
+      article.title = article.title.length > 100 ? article.title.substring(0, 97) + '...' : article.title;
+      article.description = article.description.length > 200 ? article.description.substring(0, 197) + '...' : article.description;
+    });
+    // Get total articles count
+    const totalItems = await ArticleRepository.countByUserId(userId);
+    const totalPages = Math.ceil(totalItems / limit);
+    // Return articles list
     response(res).success({ 
-      title: article.title,
-      description: article.description,
-      created_at: article.created_at
+      articles,
+      totalItems,
+      totalPages
     });
   } catch (error) {
     if (error instanceof ApiError) {
@@ -36,6 +45,5 @@ async function getArticleDetail(req, res) {
   }
 }
 module.exports = {
-  getArticleDetail,
   getArticleListByUser,
 };
--- repositories/ArticleRepository.js
+++ repositories/ArticleRepository.js
@@ -8,22 +8,32 @@ class ArticleRepository extends BaseRepository {
     return this.findById(articleId);
   }
   async getByUserId(user_id, page = 1, limit = 10) {
-    const offset = (page - 1) * limit;
-    const query = { where: { user_id }, order: { created_at: 'DESC' }, skip: offset, take: limit };
-    const [items, total] = await this.find(query);
-    const totalPages = Math.ceil(total / limit);
-    return { 
-      items: items.map(item => ({
-        ...item, 
-        title: this.trimText(item.title, 50), 
-        description: this.trimText(item.description, 100)
-      })), 
-      total, 
-      totalPages 
-    };
+    try {
+      const offset = (page - 1) * limit;
+      const query = { where: { user_id }, order: { created_at: 'DESC' }, skip: offset, take: limit };
+      const [items, total] = await this.find(query);
+      const totalPages = Math.ceil(total / limit);
+      return { 
+        items: items.map(item => ({
+          ...item, 
+          title: this.trimText(item.title, 50), 
+          description: this.trimText(item.description, 100)
+        })), 
+        total, 
+        totalPages 
+      };
+    } catch (error) {
+      console.error(`Error occurred while fetching articles by user id: ${error.message}`);
+      throw error;
+    }
   }
   trimText(text, maxLength) {
-    return text.length > maxLength ? `${text.substring(0, maxLength)}...` : text;
+    try {
+      return text.length > maxLength ? `${text.substring(0, maxLength)}...` : text;
+    } catch (error) {
+      console.error(`Error occurred while trimming text: ${error.message}`);
+      throw error;
+    }
   }
 }
 module.exports = new ArticleRepository();
