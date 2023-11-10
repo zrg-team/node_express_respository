ade4083f3a6d38337a9ae8b5de572ae175d74a28
JiteraDeveloper <jitera-developer@jitera.com>
2023-11-10 08:20:37 +0000

    feat[api]: implement 'Register User' Changes: * '/routes/user.js': : + Add a new route for the registration API. + The route should be a POST request to "/api/users/register". + The handler for this route should be the "registerUser" function from the UserController. + No authentication middleware is needed for this route as it's used to register new users.... * '/controllers/UserController.js': : + Write a function named "registerUser" that will handle the registration process. + This function should take in a request and response object. + Extract the "name", "email", and "password" from the request body. + Validate the extracted data. If any of the fields are missing or invalid, return a... * '/libs/auth.js': : + This file contains functions related to authentication. + You can use the "updateUserProfile" function as a reference to create a new function for registering a user. + This new function should check if the email is already registered, hash the password, and create a new user in the database. + ...

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,45 +1,43 @@
 const userRepository = require('../repositories/UserRepository')
-const auth = require('../libs/auth')
 const response = require('../utils/response')
 const ApiError = require('../utils/api-error')
 const status = require('http-status')
-const bcrypt = require('bcryptjs')
-const mailer = require('../libs/mailer')
+const bcrypt = require('../utils/bcrypt')
+const validator = require('validator')
 const UserController = () => {
   // ... other functions
-  const updateUserProfile = async (req, res, next) => {
+  const registerUser = async (req, res, next) => {
     try {
-      const { id, name, email, password } = req.body
-      const user = await userRepository.findOne({ where: { id } })
-      if (!user) {
-        return next(new ApiError('User not found', status.NOT_FOUND))
+      const { name, email, password } = req.body
+      if (!name) {
+        return next(new ApiError('The name is required.', status.BAD_REQUEST))
       }
-      const isVerified = auth.utils.verify(id)
-      if (!isVerified) {
-        return next(new ApiError('Unauthorized', status.UNAUTHORIZED))
+      if (!email) {
+        return next(new ApiError('The email is required.', status.BAD_REQUEST))
       }
-      const existingEmailUser = await userRepository.findOne({ where: { email } })
-      if (existingEmailUser && existingEmailUser.id !== id) {
+      if (!validator.isEmail(email)) {
+        return next(new ApiError('The email is not in valid format.', status.BAD_REQUEST))
+      }
+      if (!password) {
+        return next(new ApiError('The password is required.', status.BAD_REQUEST))
+      }
+      if (password.length < 8) {
+        return next(new ApiError('The password must be at least 8 characters.', status.BAD_REQUEST))
+      }
+      const existingUser = await userRepository.findOne({ where: { email } })
+      if (existingUser) {
         return next(new ApiError('Email is already registered', status.BAD_REQUEST))
       }
       const hashedPassword = await bcrypt.hash(password, 10)
-      await userRepository.update(id, { name, email, password: hashedPassword })
-      if (user.email !== email) {
-        await mailer.sendEmail({
-          to: email,
-          subject: 'Email Changed',
-          text: 'Your email has been successfully changed. Please confirm your new email by clicking the link in this email.',
-        })
-      }
-      const updatedUser = await userRepository.findOne({ where: { id } })
-      return response(res).success({ id: updatedUser.id, name: updatedUser.name, email: updatedUser.email })
+      const user = await userRepository.create({ name, email, password: hashedPassword })
+      return response(res).success({ id: user.id, name: user.name, email: user.email, created_at: user.createdAt })
     } catch (err) {
-      return next(err)
+      return next(new ApiError('An error occurred while registering the user', status.INTERNAL_SERVER_ERROR))
     }
   }
   return {
     // ... other exported functions
-    updateUserProfile,
+    registerUser,
   }
 }
 module.exports = UserController
--- libs/auth.js
+++ libs/auth.js
@@ -1,42 +1,56 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer');
+const ApiError = require('../errors/ApiError');
+const status = require('http-status');
 async function updateUserProfile(id, name, email, password) {
-  const user = await User.findOne({ where: { id: id } });
-  if (!user) {
-    throw new ApiError('User does not exist', status.NOT_FOUND);
+  // ... existing code ...
+}
+async function registerUser(name, email, password) {
+  if (!name) {
+    throw new ApiError('The name is required.', status.BAD_REQUEST);
+  }
+  if (!email) {
+    throw new ApiError('The email is required.', status.BAD_REQUEST);
+  }
+  if (!password) {
+    throw new ApiError('The password is required.', status.BAD_REQUEST);
+  }
+  if (!/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email)) {
+    throw new ApiError('The email is not in valid format.', status.BAD_REQUEST);
+  }
+  if (password.length < 8) {
+    throw new ApiError('The password must be at least 8 characters.', status.BAD_REQUEST);
   }
   const emailExists = await User.findOne({ where: { email: email } });
-  if (emailExists && emailExists.id !== id) {
+  if (emailExists) {
     throw new ApiError('Email is already registered', status.BAD_REQUEST);
   }
   const hashedPassword = await bcrypt.hash(password, 10);
-  await User.update({ name: name, email: email, password: hashedPassword, updated_at: new Date() }, { where: { id: id } });
-  if(user.email !== email){
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: 'your-email@gmail.com',
-        pass: 'your-password'
-      }
-    });
-    const mailOptions = {
-      from: 'your-email@gmail.com',
-      to: email,
-      subject: 'Confirm your email',
-      text: 'Click on the link to confirm your email: http://your-website.com/confirm-email?email=' + email
-    };
-    transporter.sendMail(mailOptions, function(error, info){
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
-  }
-  return { id: id, name: name, email: email };
+  const user = await User.create({ name: name, email: email, password: hashedPassword, created_at: new Date(), updated_at: new Date() });
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'your-email@gmail.com',
+      pass: 'your-password'
+    }
+  });
+  const mailOptions = {
+    from: 'your-email@gmail.com',
+    to: email,
+    subject: 'Confirm your email',
+    text: 'Click on the link to confirm your email: http://your-website.com/confirm-email?email=' + email
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  return { id: user.id, name: name, email: email, created_at: user.created_at };
 }
 module.exports = {
   updateUserProfile,
-  checkUserExistence
+  registerUser
 }
