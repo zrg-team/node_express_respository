a25cb8e978b34b78cf3ab630890958f62fdaa763
Heath (Jitera) <developers@jitera.com>
2023-11-12 14:19:00 +0000

    feat[business-logic]: implement 'Get article details' Changes: * '/controllers/ArticleController.js': : In this file, you will need to create a new method `getArticleDetails` which will handle the incoming request for fetching article details. This method will use the `id` from the request parameters to fetch the article details from the database. You can refer to the methods in UserController.js f... * '/repositories/ArticleRepository.js': : You will need to create a new file `ArticleRepository.js` which extends the `BaseRepository`. In this file, you will create a new method `getArticleById` which will interact with the database to fetch the article details based on the provided `id`. You can refer to the methods in UserRepository.j...

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -1,16 +1,28 @@
 const BaseRepository = require('../repositories/BaseRepository');
+const ArticleRepository = require('../repositories/ArticleRepository');
 const Article = require('../models/Article');
 class ArticleController {
-    ...
-    async deleteArticle(req, res) {
-        const { id } = req.params;
-        const article = await BaseRepository.findById(Article, id);
-        if (!article) {
-            return res.status(404).json({ message: 'Article not found' });
-        }
-        await BaseRepository.delete(Article, id);
-        return res.status(200).json({ message: 'Article deleted successfully' });
+  async getArticleDetails(req, res) {
+    const { id } = req.params;
+    const article = await ArticleRepository.getArticleById(id);
+    if (!article) {
+      return res.status(404).json({ message: 'Article not found' });
     }
-    ...
+    return res.json({
+      id: article.id,
+      title: article.title,
+      content: article.content,
+      created_by: article.created_by
+    });
+  }
+  async deleteArticle(req, res) {
+    const { id } = req.params;
+    const article = await BaseRepository.findById(Article, id);
+    if (!article) {
+      return res.status(404).json({ message: 'Article not found' });
+    }
+    await BaseRepository.delete(Article, id);
+    return res.status(200).json({ message: 'Article deleted successfully' });
+  }
 }
-module.exports = ArticleController;
+module.exports = new ArticleController();
--- repositories/ArticleRepository.js
+++ repositories/ArticleRepository.js
@@ -1,17 +1,27 @@
+const BaseRepository = require('./BaseRepository');
 const SequelizeUtils = require('../utils/SequelizeUtils');
-class ArticleRepository {
-    // Other functions...
-    async deleteArticle(articleId) {
-        try {
-            const article = await SequelizeUtils.findOne('articles', { id: articleId });
-            if (!article) {
-                throw new Error('Article not found');
-            }
-            await SequelizeUtils.delete('articles', { id: articleId });
-            return 'Article has been deleted successfully';
-        } catch (error) {
-            throw error;
-        }
+class ArticleRepository extends BaseRepository {
+  constructor () {
+    super('articles') // assuming 'articles' is the name of the table
+  }
+  async getArticleById(id) {
+    const article = await this.model.findByPk(id);
+    if (!article) {
+      throw new Error('Article not found');
     }
+    return article;
+  }
+  async deleteArticle(articleId) {
+    try {
+        const article = await SequelizeUtils.findOne('articles', { id: articleId });
+        if (!article) {
+            throw new Error('Article not found');
+        }
+        await SequelizeUtils.delete('articles', { id: articleId });
+        return 'Article has been deleted successfully';
+    } catch (error) {
+        throw error;
+    }
+  }
 }
 module.exports = new ArticleRepository();
