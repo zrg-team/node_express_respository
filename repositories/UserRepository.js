92417e9d2d4f9b0f83b75060574a9f5c0fdc6f41
Eadie (Jitera) <developers@jitera.com>
2023-11-13 08:35:49 +0000

    feat[business-logic]: implement 'Get article list' Changes: * '/repositories/UserRepository.js': + Write a function named "getArticlesByUserId" with input parameters "user_id" and "page". + This function should interact with the database to retrieve the list of articles associated with the "user_id". + Implement the ordering and pagination logic as described in the requirement. You can use the ...

--- repositories/UserRepository.js
+++ repositories/UserRepository.js
@@ -1,13 +1,24 @@
 const BaseRepository = require('./BaseRepository')
-
+const { Op } = require('sequelize')
+const { Article } = require('../models')
 class UserRepository extends BaseRepository {
   constructor () {
     super('user')
-
-    this.DEFAULT_LIMIT = 20
-    this.DEFAULT_SORT = [['id', 'DESC']]
+    this.DEFAULT_LIMIT = 10
+    this.DEFAULT_SORT = [['created_at', 'DESC']]
     this.DEFAULT_PAGE = 0
   }
+  async getArticlesByUserId(user_id, page = this.DEFAULT_PAGE) {
+    const offset = page * this.DEFAULT_LIMIT
+    const articles = await Article.findAll({
+      where: { user_id },
+      order: this.DEFAULT_SORT,
+      limit: this.DEFAULT_LIMIT,
+      offset
+    })
+    const totalItems = await Article.count({ where: { user_id } })
+    const totalPages = Math.ceil(totalItems / this.DEFAULT_LIMIT)
+    return { articles, totalItems, totalPages }
+  }
 }
-
 module.exports = new UserRepository()
