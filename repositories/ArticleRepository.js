139b477914e337bcc282256af512e65ce2ebcb9c
Almire (Jitera) <developers@jitera.com>
2023-11-13 09:05:24 +0000

    feat[business-logic]: implement 'Get article list' Changes: * '/repositories/ArticleRepository.js': + Create a new file named `ArticleRepository.js` in the `/repositories` directory. + This new file will extend the `BaseRepository` class and override the necessary methods. + You will need to override the `get` method to fetch the list of articles from the database, ordered by the "created_at" colu...

--- repositories/ArticleRepository.js
+++ repositories/ArticleRepository.js
@@ -1,7 +1,7 @@
 const BaseRepository = require('./BaseRepository')
 class ArticleRepository extends BaseRepository {
-  constructor () {
-    super('article')
+  constructor (model) {
+    super(model)
   }
   async getArticleDetail (id) {
     if (!id) {
@@ -13,5 +13,51 @@ class ArticleRepository extends BaseRepository {
     }
     return article
   }
+  get (filters, criteria) {
+    filters = this.prepareGetFilters({
+      ...criteria,
+      ...filters,
+      order: [['created_at', 'DESC']],
+      limit: 10,
+      offset: (filters.page - 1) * 10 || 0
+    })
+    return this.model.findAndCountAll(filters)
+      .then(result => {
+        return this.handlePaginate(result, filters)
+      })
+  }
+  handlePaginate (result, filters) {
+    return {
+      total: result.count,
+      limit: filters.limit,
+      page: filters.page || 1,
+      totalPage: Math.ceil(result.count / filters.limit),
+      data: this.formatArticles(result.rows),
+      count: result.rows.length
+    }
+  }
+  handleResult (result) {
+    if (result) {
+      return {
+        data: this.formatArticles(result),
+        count: result.length
+      }
+    }
+    return {
+      data: [],
+      count: 0
+    }
+  }
+  formatArticles (articles) {
+    return articles.map(article => ({
+      ...article,
+      title: this.trimToTwoLines(article.title),
+      description: this.trimToTwoLines(article.description)
+    }))
+  }
+  trimToTwoLines (text) {
+    const lines = text.split('\n')
+    return lines.length > 2 ? lines.slice(0, 2).join('\n') : text
+  }
 }
-module.exports = new ArticleRepository()
+module.exports = ArticleRepository
