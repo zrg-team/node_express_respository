8629649c420190aea66b30dc1b3a1713b645ca8d
Lydia (Jitera) <developers@jitera.com>
2023-11-13 08:41:11 +0000

    feat[api]: implement 'Get Article List' Changes: * '/routes/user.js': + Add a new route 'GET /api/articles' with path 'ArticleController.getArticles' and middlewares 'auth.service.all()'. + This route will call the 'getArticles' function in the 'ArticleController' which will handle the logic for fetching the articles from the database.
    ## EXAMPLE
    ```javascript
    const ...
    * '/controllers/ArticleController.js': + Implement function 'getArticles' with input "req, res" and output "res.json({ articles: articles, total_pages: totalPages, limit: limit, page: page })"
    + Use 'ArticleRepository' to fetch the articles from the database.
    + Handle pagination and search functionality.
    
    ## EXAMPLE
    ```javascript
    const A...

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -1,7 +1,29 @@
-// PATH: /controllers/ArticleController.js
-const rawRepository = require('../repositories/RawRepository')
-const response = require('../utils/response')
+const ArticleRepository = require('../repositories/ArticleRepository');
+const response = require('../utils/response');
 const ArticleController = () => {
+  const getArticles = async (req, res, next) => {
+    try {
+      const page = parseInt(req.query.page) || 1;
+      const limit = parseInt(req.query.limit) || 10;
+      if (isNaN(page) || isNaN(limit)) {
+        return response(res).error('Wrong format.', 422);
+      }
+      if (page < 1) {
+        return response(res).error('Page must be greater than 0.', 422);
+      }
+      const articles = await ArticleRepository.getArticles(page, limit);
+      const totalArticles = await ArticleRepository.countArticles();
+      const totalPages = Math.ceil(totalArticles / limit);
+      return response(res).success({
+        articles: articles,
+        total_pages: totalPages,
+        limit: limit,
+        page: page
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
   const getArticleDetail = async (req, res, next) => {
     try {
       const { user_id, article_id } = req.body;
@@ -48,7 +70,8 @@ const ArticleController = () => {
     }
   }
   return {
+    getArticles,
     getArticleDetail
   }
 }
-module.exports = ArticleController
+module.exports = ArticleController;
