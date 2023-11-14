38fc2875f9214e2680de58b26a02c4cf72ecc4bd
Calli (Jitera) <developers@jitera.com>
2023-11-14 10:56:00 +0000

    feat[database]: create table 'articles' and update table 'users' Changes * '/models/article.js': created * '/migrations/20210521161025-create-article.js': created * '/models/user.js': updated * '/models/user.js': created

---
+++ models/article.js
@@ -0,0 +1,39 @@
+module.exports = function (sequelize, DataTypes) {
+  const Article = sequelize.define('article', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {
+    tableName: 'articles'
+  })
+  Article.associate = (models) => {
+    Article.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'user'
+    })
+  }
+  return Article
+}
