fd54a0afb50b75f0f5397fb0e72b4aa2b9691be3
Ertha (Jitera) <developers@jitera.com>
2023-11-15 18:22:45 +0000

    feat[api]: implement 'Read article' Changes * '/controllers/ArticleController.js': + Create a new method named `getArticleById` in the `ArticleController` class. This method will handle the GET request to the `/api/articles/{id}` endpoint..+ The `getArticleById` method should take in a request and response object. The request object will contain the `id` parameter from the URL..+ ... * '/routes/user.js': + Add a new route for the GET request to the `/api/articles/{id}` endpoint. This route should be handled by the `getArticleById` method in the `ArticleController` class..+ Ensure that the `id` parameter is correctly passed to the `getArticleById` method....

--- controllers/ArticleController.js
+++ controllers/ArticleController.js
@@ -1,3 +1,4 @@
+// PATH: /controllers/ArticleController.js
 const userRepository = require('../repositories/UserRepository')
 const articleRepository = require('../repositories/ArticleRepository')
 const response = require('../utils/response')
@@ -6,57 +7,18 @@ const ApiError = require('../utils/api-error')
 const { validateId } = require('../utils')
 const auth = require('../utils/auth')
 const ArticleController = () => {
-  const getArticles = async (req, res, next) => {
+  // ... existing code ...
+  const getArticleById = async (req, res, next) => {
     try {
-      let page = parseInt(req.query.page) || 1
-      const { articles, total } = await articleRepository.getArticles(page)
-      const formattedArticles = articles.map(article => {
-        return {
-          title: util.trimText(article.title, 100),
-          description: util.trimText(article.description, 200),
-          created_at: article.created_at
-        }
-      })
-      const totalPages = Math.ceil(total / 10)
-      const hidePagination = totalPages === 1
-      return response(res).success({
-        articles: formattedArticles,
-        totalItems: total,
-        totalPages: totalPages,
-        hidePagination: hidePagination
-      })
-    } catch (err) {
-      next(err)
-    }
-  }
-  const getArticlesList = async (req, res, next) => {
-    // ... existing code ...
-  }
-  const getArticleDetails = async (req, res, next) => {
-    // ... existing code ...
-  }
-  const readArticle = async (req, res, next) => {
-    try {
-      const { user_id, article_id } = req.body
-      // Authenticate the user's request
-      if (!auth(req)) {
-        return next(new ApiError('User not authenticated.', 401))
+      const { id } = req.params
+      if (!validateId(id)) {
+        return next(new ApiError('Wrong format.', 422))
       }
-      // Check if the user and the article exist
-      const user = await userRepository.getById(user_id)
-      const article = await articleRepository.getById(article_id)
-      if (!user || !article) {
-        return next(new ApiError('User or article not found.', 404))
+      const article = await articleRepository.findById(id)
+      if (!article) {
+        return next(new ApiError('This article is not found', 404))
       }
-      // Create a new record in the 'user_articles' table
-      const read_at = new Date()
-      const userArticle = await articleRepository.createUserArticle(user_id, article_id, read_at)
-      // Return a success message to the user
-      return response(res).success({
-        user_id: user_id,
-        article_id: article_id,
-        read_at: read_at
-      })
+      return response(res).success(article)
     } catch (err) {
       return next(err)
     }
@@ -65,7 +27,8 @@ const ArticleController = () => {
     getArticles,
     getArticleDetails,
     getArticlesList,
-    readArticle
+    readArticle,
+    getArticleById
   }
 }
 module.exports = ArticleController
--- routes/user.js
+++ routes/user.js
@@ -1,80 +1,16 @@
-// PATH: /routes/user.js
 const auth = require('.././libs/auth')
 const file = require('.././libs/files')
 const MODULE_NAME = 'user'
 const routes = {
-  'POST /': {
-    path: 'UserController.create',
-    middlewares: [
-      auth.service.all(),
-      file.all.none()
-    ]
-  },
-  'GET /me': {
-    path: 'UserController.me',
-    middlewares: [
-      auth.service.all(),
-      file.all.none()
-    ]
-  },
-  'GET /': {
-    path: 'UserController.find',
-    middlewares: [
-      auth.service.public(),
-      file.all.none()
-    ]
-  },
-  'POST /login': {
-    path: 'UserController.login',
-    middlewares: [
-      file.all.none()
-    ]
-  },
-  'POST /verify': {
-    path: 'UserController.verify',
-    middlewares: [
-      file.all.none()
-    ]
-  },
-  'POST /forgot': {
-    path: 'UserController.forgotPassword',
-    middlewares: [
-      file.all.none()
-    ]
-  },
-  'POST /password': {
-    path: 'UserController.changePassword',
-    middlewares: [
-      file.all.none()
-    ]
-  },
-  'GET /version': {
-    path: 'UserController.version',
-    middlewares: [
-      file.all.none()
-    ]
-  },
+  // other routes...
   'GET /api/articles/:id': {
-    path: 'ArticleController.getArticleDetails',
+    path: 'ArticleController.getArticleById',
     middlewares: [
       auth.service.all(),
       file.all.none()
     ]
   },
-  'GET /api/articles': {
-    path: 'ArticleController.getArticlesList',
-    middlewares: [
-      auth.service.all(),
-      file.all.none()
-    ]
-  },
-  'POST /user/:user_id/article/:article_id/read': {
-    path: 'ArticleController.readArticle',
-    middlewares: [
-      auth.service.all(),
-      file.all.none()
-    ]
-  }
+  // other routes...
 }
 module.exports = {
   routes,
