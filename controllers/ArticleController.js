2ff06b977eb9acac531e0356f7298bbe7a855e3e
Heather (Jitera) <developers@jitera.com>
2023-11-23 20:56:21 +0000

    feat[business-logic]: implement 'Get article list' Changes * '/utils/articleHelper.js': + Create a new function named "formatArticles" which accepts an array of articles..+ Iterate over the articles, trimming the "title" and "description" fields to fit within 2 lines if necessary..+ Return the formatted articles.... * '/controllers/ArticleController.js': + After getting the articles from the ArticleRepository, pass them to the "formatArticles" function from the "articleHelper.js" utility..+ Calculate the total number of pages based on the total number of articles and the number of articles per page..+ Return a response with the list of articles, the...

---
+++ controllers/ArticleController.js
@@ -0,0 +1,51 @@
+// PATH: /controllers/ArticleController.js
+const ArticleRepository = require('../repositories/ArticleRepository')
+const response = require('../utils/response')
+const { validatePage } = require('../utils/validate')
+const validateId = require('../utils/validateId')
+const ApiError = require('../utils/api-error')
+const { formatArticles } = require('../utils/articleHelper')
+const ArticleController = () => {
+  const getArticles = async (req, res, next) => {
+    try {
+      const page = validatePage(req.query.page)
+      const { articles, total } = await ArticleRepository.getArticles(page)
+      const formattedArticles = formatArticles(articles)
+      const totalPages = Math.ceil(total / 10)
+      return response(res)
+        .success({
+          articles: formattedArticles,
+          total,
+          pages: totalPages
+        })
+    } catch (err) {
+      next(err)
+    }
+  }
+  const getArticleDetails = async (req, res, next) => {
+    try {
+      const { id } = req.params
+      // Validate id
+      if (!validateId(id)) {
+        return next(new ApiError('Invalid ID', 400))
+      }
+      // Fetch article details
+      const article = await ArticleRepository.getArticleById(id)
+      // Check if article exists
+      if (!article) {
+        return next(new ApiError('Article not found', 404))
+      }
+      // Extract required fields
+      const { title, description, created_at } = article
+      // Return article details
+      return response(res).success({ title, description, created_at })
+    } catch (err) {
+      return next(err)
+    }
+  }
+  return {
+    getArticles,
+    getArticleDetails
+  }
+}
+module.exports = ArticleController
---
+++ utils/articleHelper.js
@@ -0,0 +1,17 @@
+const trimArticleFields = (article) => {
+  const maxLength = 100; // Maximum length for 2 lines of text
+  if (article.title.length > maxLength) {
+    article.title = article.title.substring(0, maxLength) + '...';
+  }
+  if (article.description.length > maxLength) {
+    article.description = article.description.substring(0, maxLength) + '...';
+  }
+  return article;
+}
+const formatArticles = (articles) => {
+  return articles.map(article => trimArticleFields(article));
+}
+module.exports = {
+  trimArticleFields,
+  formatArticles,
+}
