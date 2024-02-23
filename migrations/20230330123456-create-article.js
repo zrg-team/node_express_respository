24aacf1f5ec5f105836bf71dd5b7faea6c3b12a1
Luce (Jitera) <developers@jitera.com>
2024-02-23 08:09:34 +0000

    feat[database]: create table 'articles' Changes * '/models/article.js': created * '/migrations/20230330123456-create-article.js': created

---
+++ migrations/20230330123456-create-article.js
@@ -0,0 +1,22 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('articles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER(11)
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('articles')
+};
