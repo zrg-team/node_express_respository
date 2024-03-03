fec9dbfd1835dc345004be766d80075b49a20031
Frayda (Jitera) <developers@jitera.com>
2024-03-03 13:13:25 +0000

    feat[database]: update table 'users' and create table 'comments' and create table 'articles' Changes * '/models/article.js': created * '/migrations/1709471548362_create_articles_table.js': created * '/models/comment.js': created * '/migrations/1709471548362_create_comments_table.js': created * '/models/user.js': updated * '/migrations/1709471519751_update_users_table.js': updated

---
+++ migrations/1709471548362_create_articles_table.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('articles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      title: Sequelize.STRING,
+      summary: Sequelize.TEXT,
+      content: Sequelize.TEXT,
+      publication_date: Sequelize.DATE,
+      category: Sequelize.STRING,
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('articles');
+  }
+};
---
+++ migrations/1709471548362_create_comments_table.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      article_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'articles',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('comments');
+  }
+};
